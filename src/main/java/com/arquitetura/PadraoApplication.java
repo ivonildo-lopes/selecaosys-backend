dfa775f2df38166d9bbd50f140f4795037aa4387
ivonildo <ivonildo@hapvida.com.br>
2018-04-11 10:54:57 -0300

    alterando o pom

--- src/main/java/com/arquitetura/PadraoApplication.java
+++ src/main/java/com/arquitetura/PadraoApplication.java
@@ -7,16 +7,20 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class PadraoApplication extends SpringBootServletInitializer {
 
+	public static void main(String[] args) {
+		new PadraoApplication().configure(new SpringApplicationBuilder(PadraoApplication.class)).run(args);
+	}
 	
+	/**
+	 * Caso queira usar no tomcat externo descomentar e alterar o pom de Jar para War
+	 */
+
 //	@Override
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //		return application.sources(PadraoApplication.class);
 //	}
 	
 	
-	public static void main(String[] args) {
-		new PadraoApplication().configure(new SpringApplicationBuilder(PadraoApplication.class)).run(args);
-	}
 
 	
 	
